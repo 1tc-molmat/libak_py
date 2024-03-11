9648db48501cce3fd5e6004167c7adebbfd7b65e
1tc-molmat <131852015+1tc-molmat@users.noreply.github.com>
2024-03-11 11:49:52 +0100

    .
    tomb kiirva

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        static void Kiir(int[] t , string cim)
+        {
+            Console.WriteLine(cim);
+            foreach (int szam in t)
+            {
+                Console.Write($"{szam}, ");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[] libak = { 1, 5, 2, 3, 4 };
+            Kiir(libak,"libak sullyai");
+            Console.WriteLine("gomb");
+            Console.ReadKey();
+
+        }
+    }
+}
